6129df0a6c29c6f2f7e41553cb19141ca39fd08e
Mike Watkins <mike.watkins@snagajob.com>
2020-03-17 14:04:28 -0400

    reworked run shell

--- util/run_shell.py
+++ util/run_shell.py
@@ -1,6 +1,5 @@
-import botocore.paginate
+import shlex
 from typing import Dict, Optional, Any, Union, List, Set
-import sys
 import subprocess
 
 import logging
@@ -8,19 +7,23 @@ import logging
 logging.getLogger(__name__)
 
 
-class RunScript:
+class RunShell:
 
     @staticmethod
-    def run_shell_script(script_path: str, env_args: Dict[str, str], execution_timeout: Optional[int]) -> int:
+    def run_command(commands: Union[str, List[str]], env_args: Dict[str, str], execution_timeout: Optional[int]) -> int:
         """
-        runs shell script as synchronous subprocess
-        :param script_path: path to script
+        runs shell command as synchronous subprocess
+        :param commands: commands to run as string or List[str], e.g. "ls -la" or ["ls", "-la"]
+                         If receiving a string, use shlex to split arguments to prevent injections
+                         This should not matter because Popen has shell=False as default, just added for extra security
         :param env_args: args to expose to shell script as environment variables
         :param execution_timeout: timeout to wait for completion before automatic failure
         :return: return code of the subprocess
         """
+        if isinstance(commands, str):
+            commands = shlex.split(commands)
         try:
-            rc = subprocess.call([script_path],
+            rc = subprocess.call(commands,
                                  env=env_args,
                                  timeout=execution_timeout)
         except OSError as os_err:
